e284f36e61a41795249202ad33f7daf026e37d0e
Pablo Fernandez <pablo_fz@hotmail.com>
2021-05-06 16:54:55 +0200

    combinacion de parametros funciona

---
+++ IPOG/combinacionParametros/combinacionParametros.js
@@ -0,0 +1,66 @@
+function combinacionParametros(m, n) {
+  let combinacion = [];
+  let combinaciones = [];
+  let fin = false;
+  let encontrado = false;
+  if ((m <= 0) | (n <= 0) | (n > m)) {
+    console.log("m, n > 0 y m >= n");
+  } else {
+    for (i = 0; i < m; i++) {
+      //generamos primera combinacion
+      if (i < n - 1) {
+        combinacion.push(0);
+      } else {
+        combinacion.push(1);
+      }
+    }
+  }
+
+  combinaciones.push(combinacion);
+
+  let g;
+  while (!fin) {
+    combinacion = [...combinacion];
+    g = m - 2;
+    encontrado = false;
+    while (!encontrado & (g >= 0)) {
+      if (combinacion[g] === 0 && combinacion[g + 1] === 1) {
+        encontrado = true;
+      } else {
+        g--;
+      }
+    }
+
+    if (combinacion[m - 1] === 1) {
+      combinacion[g] = 1;
+      combinacion[g + 1] = 0;
+    } else {
+      let r = 0;
+      for (i = 0; i <= g; i++) {
+        r += combinacion[i];
+      }
+
+      combinacion[g] = 1;
+      let ultimosUnos = n - r - 1;
+
+      for (i = g + 1; i < m - ultimosUnos; i++) {
+        combinacion[i] = 0;
+      }
+      if (ultimosUnos > 0) {
+        for (i = m - ultimosUnos; i < m; i++) {
+          combinacion[i] = 1;
+        }
+      }
+    }
+    if (encontrado) {
+      combinaciones.push(combinacion);
+    } else {
+      fin = true;
+      console.log(combinaciones);
+    }
+  }
+}
+
+combinacionParametros(5, 3);
+
+module.exports = combinacionParametros;
